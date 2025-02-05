68ec1a1b221717013fc42d0fc3844218423fadb7
Pasindu0707 <123113924+Pasindu0707@users.noreply.github.com>
2025-02-06 00:59:46 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'; 
 import './App.css';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
-  base: "/Modern_Island"
-})
+  plugins: [react(), tailwindcss()],
+  base: "/Modern_Island/", // ✅ Set this to match your repository name
+});
